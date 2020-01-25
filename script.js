001b85968d21188d02fe1c8e3ef79a182c6805e0
DivBlock <60297131+DivBlock@users.noreply.github.com>
2020-01-25 21:59:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector(".btn-menu").onclick = function() {
+
+		var ss = document.querySelector(".mmenu");
+		ss.style.left = "0%";
+
+}
+
+document.querySelector(".exit").onclick = function() {
+		var ss = document.querySelector(".mmenu");
+		ss.style.left = "-100%";
+}
+
